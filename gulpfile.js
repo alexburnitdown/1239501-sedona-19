cd7aac6257dc329e968204226e9eeda31b1742f0
Алексей <alexburnitdown@gmail.com>
2020-05-18 15:25:41 +0300

    Автоматизация 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 "use strict";
 
 var gulp = require("gulp");
+var webp = require("gulp-webp");
+var imagemin = require("gulp-imagemin");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -16,6 +22,8 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
@@ -32,6 +40,30 @@ gulp.task("server", function () {
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+  });
+
+gulp.task("webp", function () {
+ return gulp.src("source/img/**/*.{png,jpg}")
+ .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("start", gulp.series("css", "server"));
--- source/js/popup.js
+++ source/js/popup.js
@@ -7,26 +7,36 @@ var popupSuccess = document.querySelector(".form-modal--success");
 var popupSuccessClose = popupSuccess.querySelector(".form-modal__button--success");
 var phone = document.querySelector("[name=form-phone]");
 var email = document.querySelector("[name=form-email]");
+var form = document.querySelector(".form-contacts");
+var mapLink = document.querySelector(".booking__map");
+var link = document.querySelector(".socials__item--youtube");
 
-if ()
-
-sendButton.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popupError.classList.add("modal-show");
-});
-
-sendButton.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popupSuccess.classList.add("modal-show");
+form.addEventListener("submit", function(evt) {
+  if (!phone.value || !email.value) {
+    evt.preventDefault();
+    popupError.classList.add("form-modal__show");
+  } else {
+     evt.preventDefault();
+    popupSuccess.classList.add("form-modal__show");
+  }
 });
 
 popupErrorClose.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popupError.classList.remove("modal-show");
+  popupError.classList.remove("form-modal__show");
 });
 
 popupSuccessClose.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popupSuccess.classList.remove("modal-show");
+  popupSuccess.classList.remove("form-modal__show");
 });
 
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (popupError.classList.contains("form-modal__show") || popupSuccess.classList.contains("form-modal__show")) {
+      evt.preventDefault();
+      popupError.classList.remove("form-modal__show");
+      popupSuccess.classList.remove("form-modal__show");
+    }
+  }
+});
